67fcc3700cf2706f63f5ac2b730a7be1d6604e1b
amatsuki <AmatsukiUrato@gmail.com>
2019-04-09 02:07:25 +0900

    [add]chapter3の問題及び回答を追加(未完成)

---
+++ src/chapter3/main.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+import random
+# PyCharmではfromを正確に書かなければ`No module`になる
+from src.chapter3.jyanken import Jyanken
+from src.chapter3.player import Player
+
+def main():
+    print("ジャンケンを始めます")
+    jyanken = Jyanken(myself, opponent)
+    myself = Player()
+    opponent = Player()
+
+    jyanken.start()
+
+
+    my_hand = int(input())
+    opponent_hand = random.randint(1, 3)
+
+    print("自分の手:" + hand_name(my_hand))
+    print("相手の手:" + hand_name(opponent_hand))
+
+    if my_hand == opponent_hand:
+        print("draw")
+    elif (my_hand == 1 and opponent_hand == 2 or
+          my_hand == 2 and opponent_hand == 3 or
+          my_hand == 3 and opponent_hand == 1):
+        print("win")
+    else:
+        print("lose")
+
+
+def hand_name(hand_num):
+    hand_str = ""
+    if hand_num == 1:
+        hand_str = "グー"
+    elif hand_num == 2:
+        hand_str = "チョキ"
+    elif hand_num == 3:
+        hand_str = "パー"
+    else:
+        pass
+    return hand_str
+
+
+if __name__ == "__main__":
+    main()
