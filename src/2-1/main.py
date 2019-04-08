0d912f7d42a44f53667559b8ad640a91e62e67dc
amatsuki <AmatsukiUrato@gmail.com>
2019-04-09 00:35:09 +0900

    [add]問題2-1の回答を追加/問題を分離

--- src/2-1/main.py
+++ src/2-1/main.py
@@ -1,24 +1,54 @@
 # coding: utf-8
+import time
 import random
-
 
 def main():
     characters = ["RYU", "KEN"]
-    print(choose_character(characters))
-    print("end")
-
-    # print("Please type enemy's character")
-    # print(character)
-    # oppCharacter = input()
-    #
-    # if not myCharacter in characters:
-    #
-    # for character in characters:
-    #     if
+    my_name = "Player"
+    opp_name = "Computer"
+    print(my_name + " Turn")
+    my_character = choose_character(characters, my_name)
+    print(opp_name + " Turn")
+    opp_character = choose_character(characters, opp_name)
 
 
-def choose_character(characters):
-    print("Please select character")
+    my_hp = 100
+    opp_hp = 100
+    round = 1
+    while(True):
+        print("==========----------")
+        print("ROUND " + str(round))
+        print("----------==========")
+        time.sleep(0.5)
+        print(">>>>>>>>>>>>>>>>>>>>")
+        print("READY TO FIGHT")
+        print(">>>>>>>>>>>>>>>>>>>>")
+        time.sleep(0.5)
+        my_atk = random.randint(0, 20)
+        opp_atk = random.randint(0, 20)
+
+        my_hp -= opp_atk
+        opp_hp -= my_atk
+        print( my_name + " HP is " + str(my_hp))
+        print( opp_name + " HP is " + str(opp_hp))
+
+        time.sleep(0.1)
+        if my_hp <= 0 and opp_hp <= 0:
+            print("DRAW")
+            break
+
+        if my_hp <= 0:
+            print(opp_name + " WIN!")
+            break
+
+        if opp_hp <= 0:
+            print(my_name + " WIN!")
+            break
+        round += 1
+        print()
+
+def choose_character(characters, player_name):
+    print("Please select " + player_name + " character")
     print(characters)
     selected_character = input()
     print("selected: " + selected_character)
@@ -27,7 +57,7 @@ def choose_character(characters):
         return selected_character
     else:
         print("Your select character isn't correct")
-        choose_character(characters)
+        choose_character(characters, player_name)
 
 
 if __name__ == "__main__":
