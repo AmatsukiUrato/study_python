e19627d9eaececef3a6138c07fdee26fab0458ad
Amatsuki <AmatsukiUrato@yahoo.co.jp>
2019-05-08 04:50:59 +0900

    chapter5の問題(何が出力されるかを当てる)を作成した

---
+++ chapter5/module.py
@@ -0,0 +1,12 @@
+def print_result(str="end", is_decorate=False):
+    if is_decorate:
+        print("***")
+        print(str)
+        print(len(str))
+        print("***")
+    else:
+        print(str)
+        print(len(str))
+    print(5)
+
+print(6)
